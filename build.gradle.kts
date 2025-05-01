369ef22a0bbdff88b58bea53bdc44463c460cc32
jsw9436 <jswjsw101010@gmail.com>
2025-05-02 01:19:06 +0900

    :memo: :: update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,28 @@
+@file:Suppress("DEPRECATION")
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+}
+
+allprojects {
+    tasks.withType(org.jetbrains.kotlin.gradle.dsl.KotlinCompile::class.java).configureEach {
+        kotlinOptions {
+            if (project.findProperty("enableMultiModuleComposeReports") == "true") {
+                freeCompilerArgs += listOf("-P", "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" + rootProject.buildDir.absolutePath + "/compose_metrics/")
+                freeCompilerArgs += listOf("-P", "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" + rootProject.buildDir.absolutePath + "/compose_metrics/")
+            }
+        }
+    }
 }
