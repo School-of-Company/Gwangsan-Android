0cd35653f37116c818a24d44a3f4651a35ffba47
kimjeakwan <s24042@gsm.hs.kr>
2025-05-03 21:07:28 +0900

    :lipstick: :: GwangSanTypography

---
+++ core/design-system/src/main/java/com/kim/design_system/theme/GwangSanTypography.kt
@@ -0,0 +1,102 @@
+package com.kim.design_system.theme
+
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.school_of_company.design_system.R
+
+object GwangSanTypography {
+    private val pretendard = FontFamily(
+        Font(R.font.pretendard_bold, FontWeight.Bold),
+        Font(R.font.pretendard_extrabold, FontWeight.ExtraBold),
+        Font(R.font.pretendard_extralight, FontWeight.ExtraLight),
+        Font(R.font.pretendard_light, FontWeight.Light),
+        Font(R.font.pretendard_medium, FontWeight.Medium),
+        Font(R.font.pretendard_regular, FontWeight.Normal),
+        Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+        Font(R.font.pretendard_thin, FontWeight.Thin),
+    )
+
+    @Stable
+    val titleLarge = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 30.sp,
+        lineHeight = 36.sp
+    )
+
+    @Stable
+    val titleMedium2 = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 36.sp,
+        lineHeight = 31.2.sp
+    )
+
+    @Stable
+    val titleSmall = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 26.sp
+    )
+
+    @Stable
+    val body1 = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        lineHeight = 25.2.sp
+    )
+
+    @Stable
+    val body2 = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 25.2.sp
+    )
+
+    @Stable
+    val body3 = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 22.4.sp
+    )
+
+    @Stable
+    val body4 = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 22.4.sp
+    )
+
+    @Stable
+    val body5 = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 19.6.sp
+    )
+
+    @Stable
+    val laBal = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 19.6.sp
+    )
+
+    @Stable
+    val caption = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.8.sp
+    )
+}
