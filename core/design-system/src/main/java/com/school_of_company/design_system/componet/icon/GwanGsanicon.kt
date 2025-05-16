ccd3cba9c72e85e5cb3c6cbb68af5b7ec0289b15
kimjeakwan <s24042@gsm.hs.kr>
2025-05-16 11:11:51 +0900

    :lipstick: :: GwanGsanicon

---
+++ core/design-system/src/main/java/com/school_of_company/design_system/componet/icon/GwanGsanicon.kt
@@ -0,0 +1,32 @@
+package com.school_of_company.design_system.componet.icon
+
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.school_of_company.design_system.R
+
+@Composable
+fun DownArrowIcon(
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.frame_276),
+        contentDescription = "뒤로가기",
+        modifier = modifier,
+        tint = tint
+    )
+}
+
+@Composable
+fun SearchIcon(
+    modifier: Modifier = Modifier,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.search_icon),
+        contentDescription = "검색",
+        modifier = modifier,
+    )
+}
