90131cd9be2ddefe20feb25a99b87c3819ac2946
kimjeakwan <s24042@gsm.hs.kr>
2025-05-16 11:13:15 +0900

    :lipstick: :: GwangSanTextField 에 searchField 추가

--- core/design-system/src/main/java/com/school_of_company/design_system/componet/textfield/GwangSanTextField.kt
+++ core/design-system/src/main/java/com/school_of_company/design_system/componet/textfield/GwangSanTextField.kt
@@ -1,9 +1,11 @@
 package com.yourpackage.design_system.component.textField
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -12,8 +14,18 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import com.school_of_company.design_system.componet.icon.SearchIcon
 import com.school_of_company.design_system.theme.GwangSanTheme
 import com.school_of_company.design_system.theme.color.GwangSanColor
+import kotlinx.coroutines.delay
 
 @Composable
 fun GwangSanTextField(
@@ -104,6 +116,99 @@ fun GwangSanTextField(
         }
     }
 }
+@Composable
+fun GwangSanSearchTextField(
+    modifier: Modifier = Modifier,
+    debounceTime: Long = 300L,
+    placeHolder: String,
+    readOnly: Boolean = false,
+    isError: Boolean = false,
+    focusManager: FocusManager = LocalFocusManager.current,
+    focusRequester: FocusRequester = FocusRequester(),
+    setText: String,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    maxLines: Int = Int.MAX_VALUE,
+    singleLine: Boolean = false,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    onValueChange: (String) -> Unit = {},
+    onSearchTextChange: (String) -> Unit = {}
+) {
+
+    GwangSanTheme { colors, typography ->
+        val isFocused = remember { mutableStateOf(false) }
+
+        DisposableEffect(Unit) {
+            onDispose {
+                focusManager.clearFocus()
+            }
+        }
+
+        LaunchedEffect(setText) {
+            delay(debounceTime)
+            onSearchTextChange(setText)
+        }
+
+        Column {
+            TextField(
+                value = setText,
+                onValueChange = {
+                    val filteredText = it.filterNot { text -> text.isWhitespace() }
+                    onValueChange(filteredText)
+                },
+                keyboardOptions = keyboardOptions,
+                keyboardActions = keyboardActions,
+                placeholder = {
+                    Text(
+                        text = placeHolder,
+                        style = typography.body5,
+                        color = colors.gray400
+                    )
+                },
+                modifier = modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(colors.white)
+                    .border(
+                        width = 1.dp,
+                        color = when {
+                            isError -> GwangSanColor.error
+                            isFocused.value -> GwangSanColor.subYellow500
+                            setText.isNotEmpty() -> GwangSanColor.subYellow500
+                            else -> GwangSanColor.gray400
+                        },
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .onFocusChanged {
+                        isFocused.value = it.isFocused
+                    },
+                maxLines = maxLines,
+                singleLine = singleLine,
+                textStyle = typography.titleMedium2,
+                colors = TextFieldDefaults.colors(
+                    focusedTextColor = GwangSanColor.black,
+                    unfocusedTextColor = GwangSanColor.black,
+                    focusedPlaceholderColor = GwangSanColor.gray400,
+                    unfocusedPlaceholderColor = GwangSanColor.gray400,
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    cursorColor = GwangSanColor.subYellow500,
+                    focusedIndicatorColor = GwangSanColor.subYellow500,
+                    unfocusedIndicatorColor = GwangSanColor.gray400,
+                    disabledIndicatorColor = GwangSanColor.gray400
+                ),
+                trailingIcon = {
+                    SearchIcon()
+                },
+                readOnly = readOnly,
+                visualTransformation = visualTransformation
+            )
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
