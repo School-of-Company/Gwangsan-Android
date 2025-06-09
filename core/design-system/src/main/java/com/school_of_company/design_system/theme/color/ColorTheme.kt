c18e8eba68d37c6d55fbbe2bdf0d7776e01ed74b
jsw9436 <jswjsw101010@gmail.com>
2025-06-09 10:00:39 +0900

    :lipstick: :: add background color

--- core/design-system/src/main/java/com/school_of_company/design_system/theme/color/ColorTheme.kt
+++ core/design-system/src/main/java/com/school_of_company/design_system/theme/color/ColorTheme.kt
@@ -50,4 +50,8 @@ abstract class ColorTheme {
     // Black And White ColorTheme
     abstract val black: Color
     abstract val white: Color
+
+    // check background color
+    abstract val background: Color
+
 }
