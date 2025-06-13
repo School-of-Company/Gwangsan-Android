e3c71f9ceaa9d55d9ddbf6b77ee64591c1a91cbd
jsw9436 <jswjsw101010@gmail.com>
2025-06-13 15:39:22 +0900

    :recycle: :: review reflection

--- app/src/main/java/com/example/gwangsan_android/navigation/TopLevelDestination.kt
+++ app/src/main/java/com/example/gwangsan_android/navigation/TopLevelDestination.kt
@@ -1,31 +1,33 @@
 package com.example.gwangsan_android.navigation
 
+import com.school_of_company.design_system.R
+
 enum class TopLevelDestination(
     val unSelectedIcon: Int,
     val iconText: String,
 ) {
     HOME(
-        unSelectedIcon = com.school_of_company.design_system.R.drawable.home,
+        unSelectedIcon = R.drawable.home,
         iconText = "홈",
     ),
 
     POST(
-        unSelectedIcon = com.school_of_company.design_system.R.drawable.copy,
+        unSelectedIcon = R.drawable.copy,
         iconText = "게시글",
     ),
 
     CHAT(
-        unSelectedIcon = com.school_of_company.design_system.R.drawable.chat,
+        unSelectedIcon = R.drawable.chat,
         iconText = "채팅",
     ),
 
     NOTIFICATION(
-        unSelectedIcon = com.school_of_company.design_system.R.drawable.horn,
+        unSelectedIcon = R.drawable.horn,
         iconText = "공지",
     ),
 
     PROFILE(
-        unSelectedIcon = com.school_of_company.design_system.R.drawable.person,
+        unSelectedIcon = R.drawable.person,
         iconText = "프로필",
     )
 }
