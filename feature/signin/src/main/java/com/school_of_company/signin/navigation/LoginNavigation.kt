94fd95ef6f340a5802c4c859d351b0d63dd1477b
jsw9436 <jswjsw101010@gmail.com>
2025-06-13 15:29:49 +0900

    :sparkles: :: create LoginNavigation

---
+++ feature/signin/src/main/java/com/school_of_company/signin/navigation/LoginNavigation.kt
@@ -0,0 +1,44 @@
+package com.school_of_company.signin.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.school_of_company.signin.view.SignInRoute
+import com.school_of_company.signin.view.StartRoute
+
+const val StartRoute = "Start_route"
+const val SignInRoute = "Sign_in_route"
+
+fun NavController.navigateToStart(navOptions: NavOptions? = null) {
+    this.navigate(StartRoute, navOptions)
+}
+
+fun NavGraphBuilder.startScreen(
+    onSignUpClick: () -> Unit,
+    onInputLoginClick: () -> Unit,
+) {
+    composable(route = StartRoute) {
+        StartRoute(
+            onSignUpClick = onSignUpClick,
+            onInputLoginClick = onInputLoginClick
+        )
+    }
+}
+
+fun NavController.navigateToSignIn(navOptions: NavOptions? = null) {
+    this.navigate( SignInRoute, navOptions)
+}
+
+fun NavGraphBuilder.signInScreen(
+    onBackClick: () -> Unit,
+    onMainClick: () -> Unit,
+) {
+    composable(route = SignInRoute) {
+        SignInRoute(
+            onBackClick = onBackClick,
+            onMainClick = onMainClick,
+        )
+    }
+}
+
