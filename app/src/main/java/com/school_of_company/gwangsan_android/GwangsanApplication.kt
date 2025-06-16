e48bed7173b3e9d5277567d0767a9000ec1138ba
kimjeakwan <s24042@gsm.hs.kr>
2025-06-16 10:33:41 +0900

    :sparkles: :: GwangsanApplication.kt

---
+++ app/src/main/java/com/school_of_company/gwangsan_android/GwangsanApplication.kt
@@ -0,0 +1,10 @@
+package com.school_of_company.gwangsan_android
+
+
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GwangsanApplication : Application() {
+}
