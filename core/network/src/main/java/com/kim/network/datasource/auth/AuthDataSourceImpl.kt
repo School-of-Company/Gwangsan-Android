4696d8c2bb54afd328dc64a13c755a71ae2511b2
kimjeakwan <s24042@gsm.hs.kr>
2025-06-18 20:14:32 +0900

    :sparkles: :: AuthDataSourceImpl.kt

---
+++ core/network/src/main/java/com/kim/network/datasource/auth/AuthDataSourceImpl.kt
@@ -0,0 +1,20 @@
+package com.kim.network.datasource.auth
+
+import com.school_of_company.network.api.AuthAPI
+import com.school_of_company.network.datasource.auth.AuthDataSource
+import com.kim.network.dto.auth.requset.LoginRequest
+import com.kim.network.dto.auth.requset.SignUpRequest
+import com.school_of_company.network.util.performApiRequest
+import javax.inject.Inject
+
+class AuthDataSourceImpl @Inject constructor(
+    private val authAPI: AuthAPI
+) : AuthDataSource {
+    override suspend fun signUp(body: SignUpRequest) = performApiRequest { authAPI.signUp(body) }
+
+    override suspend fun login(body: LoginRequest) = performApiRequest { authAPI.login(body) }
+    override suspend fun tokenRefresh(refreshToken: String) = performApiRequest { authAPI.tokenRefresh(refreshToken) }
+
+    override suspend fun sIgnLogout() = performApiRequest { authAPI.sIgnLogout() }
+    override suspend fun logout() = performApiRequest { authAPI.logout() }
+}
