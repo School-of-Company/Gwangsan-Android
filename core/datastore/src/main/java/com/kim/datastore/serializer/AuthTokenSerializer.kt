d6af4f9beaf03d2665b3e46d57bfe4f3c0841fab
kimjeakwan <s24042@gsm.hs.kr>
2025-06-18 20:21:48 +0900

    :sparkles: :: AuthTokenSerializer.kt

---
+++ core/datastore/src/main/java/com/kim/datastore/serializer/AuthTokenSerializer.kt
@@ -0,0 +1,25 @@
+package com.kim.datastore.serializer
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+
+import com.google.protobuf.InvalidProtocolBufferException
+import com.kim.datastore.AuthToken
+import java.io.InputStream
+import java.io.OutputStream
+import javax.inject.Inject
+
+class AuthTokenSerializer @Inject constructor() : Serializer<AuthToken> {
+    override val defaultValue: AuthToken = AuthToken.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): AuthToken =
+        try {
+            AuthToken.parseFrom(input)
+        } catch (e: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto", e)
+        }
+
+    override suspend fun writeTo(t: AuthToken, output: OutputStream) {
+        t.writeTo(output)
+    }
+}
