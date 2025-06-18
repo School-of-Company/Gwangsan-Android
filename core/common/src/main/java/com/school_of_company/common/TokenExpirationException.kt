eaf97c7692adde7890573ee924cd45ca8af9c855
kimjeakwan <s24042@gsm.hs.kr>
2025-06-18 20:35:48 +0900

    :sparkles: :: TokenExpirationException.kt

---
+++ core/common/src/main/java/com/school_of_company/common/TokenExpirationException.kt
@@ -0,0 +1,8 @@
+package com.school_of_company.common
+
+import java.io.IOException
+
+class TokenExpirationException : IOException() {
+    override val message: String
+        get() = "토큰이 만료되었습니다. 다시 로그인 해주세요"
+}
