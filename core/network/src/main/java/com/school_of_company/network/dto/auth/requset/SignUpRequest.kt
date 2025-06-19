fd792d30a3255bd7867ab78a5399d26d64e30ad5
kimjeakwan <s24042@gsm.hs.kr>
2025-06-19 18:26:28 +0900

    :recycle: ::  변경된 enum 클래스에 맞게 이름 변경

--- core/network/src/main/java/com/school_of_company/network/dto/auth/requset/SignUpRequest.kt
+++ core/network/src/main/java/com/school_of_company/network/dto/auth/requset/SignUpRequest.kt
@@ -1,8 +1,8 @@
 package com.school_of_company.network.dto.auth.requset
 
-import com.school_of_company.model.enum.branch
-import com.school_of_company.model.enum.dong
-import com.school_of_company.model.enum.specialty
+import com.school_of_company.model.enum.Branch
+import com.school_of_company.model.enum.Dong
+import com.school_of_company.model.enum.Specialty
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
@@ -12,9 +12,9 @@ data class SignUpRequest(
     @Json(name = "password") val password: String,
     @Json(name = "phoneNumber") val phoneNumber: String,
     @Json(name = "verificationCode") val verificationCode: String,
-    @Json(name = "dong") val dong: dong,
-    @Json(name = "branch") val branch: branch,
+    @Json(name = "dong") val dong: Dong,
+    @Json(name = "branch") val branch: Branch,
     @Json(name = "recommender") val recommender: String,
-    @Json(name = "specialty") val specialty: specialty,
+    @Json(name = "specialty") val specialty: Specialty,
     @Json(name = "introduction") val introduction: String
 )
