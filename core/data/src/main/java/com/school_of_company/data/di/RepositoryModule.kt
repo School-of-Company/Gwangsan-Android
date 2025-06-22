be31659c72103883b5508889d8f815ef538f9a63
kimjeakwan <s24042@gsm.hs.kr>
2025-06-22 17:14:12 +0900

    :sparkles: :: RepositoryModule 추가

---
+++ core/data/src/main/java/com/school_of_company/data/di/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.school_of_company.data.di
+
+import com.school_of_company.data.repository.AuthRepository
+import com.school_of_company.data.repository.AuthRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    abstract fun bindAuthRepository(
+        authRepositoryImpl: AuthRepositoryImpl
+    ): AuthRepository
+}
