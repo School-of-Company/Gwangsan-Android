d66729ec8880a151eeea37a1eb42991b2c177749
kimjeakwan <s24042@gsm.hs.kr>
2025-06-22 17:19:32 +0900

    :recycle: :: JsonClass(generateAdapter = true) 추가

--- core/network/src/main/java/com/school_of_company/network/dto/auth/reponse/LoginResponse.kt
+++ core/network/src/main/java/com/school_of_company/network/dto/auth/reponse/LoginResponse.kt
@@ -1,7 +1,9 @@
 package com.school_of_company.network.dto.reponse
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class LoginResponse(
     @Json(name = "accessToken") val accessToken: String,
     @Json(name = "refreshToken") val refreshToken: String,
