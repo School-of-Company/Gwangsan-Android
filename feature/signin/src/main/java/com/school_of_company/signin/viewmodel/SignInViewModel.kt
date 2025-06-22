88546b4feb11f22ca711758257029bf168ee4559
kimjeakwan <s24042@gsm.hs.kr>
2025-06-22 17:15:04 +0900

    :sparkles: :: SignInViewModel 추가

---
+++ feature/signin/src/main/java/com/school_of_company/signin/viewmodel/SignInViewModel.kt
@@ -0,0 +1,75 @@
+package com.school_of_company.signin.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.school_of_company.Regex.isValidId
+import com.school_of_company.Regex.isValidPassword
+import com.school_of_company.data.repository.AuthRepository
+import com.school_of_company.model.auth.request.LoginRequestModel
+import com.school_of_company.model.auth.response.LoginResponseModel
+import com.school_of_company.network.errorHandling
+import com.school_of_company.result.asResult
+import com.school_of_company.result.Result
+import com.school_of_company.signin.viewmodel.uistate.SaveTokenUiState
+import com.school_of_company.signin.viewmodel.uistate.SignInUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+internal class SignInViewModel @Inject constructor(
+    private val authRepository: AuthRepository,
+    private val savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+    companion object {
+        private const val ID = "id"
+        private const val PASSWORD = "password"
+    }
+
+    private val _signInUiState = MutableStateFlow<SignInUiState>(SignInUiState.Loading)
+    internal val signInUiState = _signInUiState.asStateFlow()
+
+    private val _saveTokenUiState = MutableStateFlow<SaveTokenUiState>(SaveTokenUiState.Loading)
+    internal val saveTokenUiState = _saveTokenUiState.asStateFlow()
+
+    internal var id = savedStateHandle.getStateFlow(key = ID, initialValue = "")
+    internal var password = savedStateHandle.getStateFlow(key = PASSWORD, initialValue = "")
+
+    internal fun login(body: LoginRequestModel) = viewModelScope.launch {
+        if (!isValidId(body.nickname)) {
+            _signInUiState.value = SignInUiState.IdNotValid
+        } else {
+            authRepository.signIn(body = body)
+                .asResult()
+                .collectLatest { result ->
+                    when (result) {
+                        is Result.Loading -> _signInUiState.value = SignInUiState.Loading
+                        is Result.Success -> {
+                            _signInUiState.value = SignInUiState.Success
+                            authRepository.saveToken(result.data)
+                        }
+                        is Result.Error -> {
+                            _signInUiState.value = SignInUiState.Error(result.exception)
+                            result.exception.errorHandling(
+                                badRequestAction = { _signInUiState.value = SignInUiState.BadRequest },
+                                notFoundAction = { _signInUiState.value = SignInUiState.NotFound }
+                            )
+                        }
+                    }
+                }
+        }
+    }
+
+    internal fun onIdChange(value: String) {
+        savedStateHandle[ID] = value
+    }
+
+    internal fun onPasswordChange(value: String) {
+        savedStateHandle[PASSWORD] = value
+    }
+}
+
