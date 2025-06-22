6f810088610473630c5f2df1546be3f743477d70
kimjeakwan <s24042@gsm.hs.kr>
2025-06-22 19:13:53 +0900

    :recycle: :: 공백

--- core/network/src/main/java/com/school_of_company/network/mapper/auth/request/LoginRequestMapper.kt
+++ core/network/src/main/java/com/school_of_company/network/mapper/auth/request/LoginRequestMapper.kt
@@ -3,7 +3,6 @@ package com.school_of_company.network.mapper.auth.request
 import com.school_of_company.model.auth.request.LoginRequestModel
 import com.school_of_company.network.dto.auth.requset.LoginRequest
 
-
 fun LoginRequestModel.toDto(): LoginRequest =
     LoginRequest(
         nickname = nickname,
