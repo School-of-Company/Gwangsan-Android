ea1212c1add0e39b9159b9039a3e3a1a917d3fcf
kimjeakwan <s24042@gsm.hs.kr>
2025-06-25 21:07:55 +0900

    :sparkles: :: SignUpViewModel.kt 추가

---
+++ feature/signup/src/main/java/com/school_of_company/signup/viewmodel/SignUpViewModel.kt
@@ -0,0 +1,204 @@
+package com.school_of_company.signup.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.school_of_company.Regex.isValidPassword
+import com.school_of_company.Regex.isValidPhoneNumber
+import com.school_of_company.data.repository.auth.AuthRepository
+import com.school_of_company.model.auth.request.SignUpCertificationNumberSendRequestModel
+import com.school_of_company.model.auth.request.SignUpRequestModel
+import com.school_of_company.network.errorHandling
+import com.school_of_company.result.asResult
+import com.school_of_company.signup.viewmodel.uistate.SendNumberUiState
+import com.school_of_company.signup.viewmodel.uistate.SignUpUiState
+import com.school_of_company.signup.viewmodel.uistate.VerifyNumberUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val authRepository: AuthRepository,
+    private val savedStateHandle: SavedStateHandle,
+): ViewModel() {
+    companion object {
+        private const val NAME = "name"
+        private const val NICKNAME = "nickname"
+        private const val PASSWORD = "password"
+        private const val RE_PASSWORD = "rePassword"
+        private const val PHONE_NUMBER = "phoneNumber"
+        private const val CERTIFICATION_NUMBER = "certificationNumber"
+        private const val DONG = "dong"
+        private const val BRANCH = "branch"
+        private const val RECOMMENDER = "recommender"
+        private const val SPECIALTY = "specialty"
+    }
+
+
+    private val _signUpUiState = MutableStateFlow<SignUpUiState>(SignUpUiState.Loading)
+    internal val signUpUiState = _signUpUiState.asStateFlow()
+
+    private val _sendNumberUiState = MutableStateFlow<SendNumberUiState>(SendNumberUiState.Loading)
+    internal val sendNumberUiState = _sendNumberUiState.asStateFlow()
+
+    private val _verifyNumberUiState = MutableStateFlow<VerifyNumberUiState>(VerifyNumberUiState.Loading)
+    internal val verifyNumberUiState = _verifyNumberUiState.asStateFlow()
+
+    internal var name = savedStateHandle.getStateFlow(key = NAME, initialValue = "")
+    internal var nickname = savedStateHandle.getStateFlow(key = NICKNAME, initialValue = "")
+    internal var certificationNumber = savedStateHandle.getStateFlow(key = CERTIFICATION_NUMBER,initialValue =  "")
+    internal var number = savedStateHandle.getStateFlow(key = PHONE_NUMBER, initialValue =  "")
+    internal var password = savedStateHandle.getStateFlow(key = PASSWORD, initialValue =  "")
+    internal var checkPassword = savedStateHandle.getStateFlow(key = RE_PASSWORD, initialValue = "")
+    internal var dong = savedStateHandle.getStateFlow(key = DONG, initialValue = "")
+    internal var branch = savedStateHandle.getStateFlow(key = BRANCH,initialValue =  "")
+    internal var recommender = savedStateHandle.getStateFlow(key = RECOMMENDER, initialValue =  "")
+    internal var specialty = savedStateHandle.getStateFlow(key = SPECIALTY, initialValue =  "")
+
+    internal fun signUp(body: SignUpRequestModel) = viewModelScope.launch {
+        _signUpUiState.value = SignUpUiState.Loading
+        when {
+            password.value != checkPassword.value -> {
+                _signUpUiState.value = SignUpUiState.PasswordMismatch
+            }
+
+            !isValidPassword(body.password) -> {
+                _signUpUiState.value = SignUpUiState.PasswordNotValid
+            }
+
+            else -> {
+                _signUpUiState.value = SignUpUiState.Loading
+                authRepository.signUp(body = body)
+                    .asResult()
+                    .collectLatest { result ->
+                        when (result) {
+                            is com.school_of_company.result.Result.Success -> _signUpUiState.value =
+                                SignUpUiState.Success
+
+                            is com.school_of_company.result.Result.Error -> {
+                                _signUpUiState.value = SignUpUiState.Error(result.exception)
+                                result.exception.errorHandling(
+                                    badRequestAction = { SignUpUiState.BadRequest },
+                                    notFoundAction = { SignUpUiState.NotFound },
+                                    tooManyRequestAction = { SignUpUiState.TooManyRequest },
+                                    conflictAction = { SignUpUiState.Conflict }
+                                )
+                            }
+
+                            is com.school_of_company.result.Result.Loading -> _signUpUiState.value = SignUpUiState.Loading
+                        }
+                    }
+            }
+        }
+    }
+
+    internal fun verifyNumber(phoneNumber: String, code: String) =
+        viewModelScope.launch {
+            _verifyNumberUiState.value = VerifyNumberUiState.Loading
+            authRepository.signUpCertificationNumberCertification(phoneNumber = phoneNumber, code = code)
+                .asResult()
+                .collectLatest { result ->
+                    when (result) {
+                        is com.school_of_company.result.Result.Loading -> _verifyNumberUiState.value = VerifyNumberUiState.Loading
+                        is com.school_of_company.result.Result.Success -> _verifyNumberUiState.value = VerifyNumberUiState.Success
+                        is com.school_of_company.result.Result.Error ->{ _verifyNumberUiState.value =  VerifyNumberUiState.Error(result.exception)
+                            result.exception.errorHandling(
+                                badRequestAction = { SignUpUiState.BadRequest },
+                                notFoundAction = { SignUpUiState.NotFound },
+                                tooManyRequestAction = { SignUpUiState.TooManyRequest },
+                                conflictAction = { SignUpUiState.Conflict }
+                            )
+                        }
+                    }
+                }
+        }
+
+    internal fun sendCertificationCode(body: SignUpCertificationNumberSendRequestModel) =
+        viewModelScope.launch {
+            _sendNumberUiState.value = SendNumberUiState.Loading
+            authRepository.signUpCertificationNumberSend(body = body)
+                .asResult()
+                .collectLatest { result ->
+                    if (!isValidPhoneNumber(body.phoneNumber)) {
+                        _sendNumberUiState.value = SendNumberUiState.PhoneNumberNotValid
+                    } else {
+                        when (result) {
+                            is com.school_of_company.result.Result.Loading -> _sendNumberUiState.value =
+                                SendNumberUiState.Loading
+
+                            is com.school_of_company.result.Result.Success -> _sendNumberUiState.value =
+                                SendNumberUiState.Success
+
+                            is com.school_of_company.result.Result.Error -> {
+                                _sendNumberUiState.value = SendNumberUiState.Error(result.exception)
+                                result.exception.errorHandling(
+                                    badRequestAction = { SignUpUiState.BadRequest },
+                                    notFoundAction = { SignUpUiState.NotFound },
+                                    tooManyRequestAction = { SignUpUiState.TooManyRequest },
+                                    conflictAction = { SignUpUiState.Conflict })
+                            }
+                        }
+                    }
+                }
+        }
+
+    private val allAreas = listOf(
+        "enw", "chiki", "첨단 3동", "첨단 4동", "첨단 5동"
+    )
+
+    val filteredAreas = dong.map { keyword ->
+        if (keyword.isBlank()) emptyList()
+        else allAreas.filter { it.contains(keyword) }
+    }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
+
+    fun onAreaSelected(value: String) {
+        savedStateHandle[DONG] = value
+    }
+
+    internal fun onNameChange(value: String) {
+        savedStateHandle[NAME] = value
+    }
+
+    internal fun onNicknameChange(value: String) {
+        savedStateHandle[NICKNAME] = value
+    }
+
+    internal fun onPasswordChange(value: String) {
+        savedStateHandle[PASSWORD] = value
+    }
+
+    internal fun onCheckPasswordChange(value: String) {
+        savedStateHandle[RE_PASSWORD] = value
+    }
+
+    internal fun onNumberChange(value: String) {
+        savedStateHandle[PHONE_NUMBER] = value
+    }
+
+    internal fun onCertificationNumberChange(value: String) {
+        savedStateHandle[CERTIFICATION_NUMBER] = value
+    }
+
+    internal fun onDongChange(value: String) {
+        savedStateHandle[DONG] = value
+    }
+
+    internal fun onBranchChange(value: String) {
+        savedStateHandle[BRANCH] = value
+    }
+
+    internal fun onRecommenderChange(value: String) {
+        savedStateHandle[RECOMMENDER] = value
+    }
+
+    internal fun onSpecialtyChange(value: String) {
+        savedStateHandle[SPECIALTY] = value
+    }
+}
