66ac4c7b6b38a5c532a253d1e6cc1a8210bc6b08
kimjeakwan <s24042@gsm.hs.kr>
2025-06-25 20:55:17 +0900

    :recycle: :: AuthDataSource.kt에 인증 추가

--- core/network/src/main/java/com/school_of_company/network/datasource/auth/AuthDataSource.kt
+++ core/network/src/main/java/com/school_of_company/network/datasource/auth/AuthDataSource.kt
@@ -1,6 +1,7 @@
 package com.school_of_company.network.datasource.auth
 
 import com.school_of_company.network.dto.auth.requset.LoginRequest
+import com.school_of_company.network.dto.auth.requset.SignUpCertificationNumberSendRequest
 import com.school_of_company.network.dto.auth.requset.SignUpRequest
 import com.school_of_company.network.dto.reponse.LoginResponse
 import kotlinx.coroutines.flow.Flow
@@ -15,4 +16,8 @@ interface AuthDataSource {
     fun signLogout(): Flow<Unit>
 
     fun logout(): Flow<Unit>
+
+    fun signUpCertificationNumberCertification(phoneNumber: String, code: String) : Flow<Unit>
+
+    fun signUpCertificationNumberSend(body: SignUpCertificationNumberSendRequest) : Flow<Unit>
 }
