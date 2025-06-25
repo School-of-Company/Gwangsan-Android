287a866a4d46f1c2da1e56c71b7e4d810efdfa98
kimjeakwan <s24042@gsm.hs.kr>
2025-06-25 21:01:41 +0900

    :recycle: :: Route단 추가

--- feature/signup/src/main/java/com/school_of_company/signup/view/PasswordSignupScreen.kt
+++ feature/signup/src/main/java/com/school_of_company/signup/view/PasswordSignupScreen.kt
@@ -1,5 +1,6 @@
 package com.school_of_company.signup.view
 
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
@@ -24,38 +25,51 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.school_of_company.Regex.isValidPassword
 import com.school_of_company.design_system.componet.button.GwangSanStateButton
 import com.school_of_company.design_system.componet.button.state.ButtonState
 import com.school_of_company.design_system.componet.clickable.GwangSanClickable
 import com.school_of_company.design_system.componet.icons.DownArrowIcon
 import com.school_of_company.design_system.componet.topbar.GwangSanTopBar
 import com.school_of_company.design_system.theme.GwangSanTheme
+import com.school_of_company.signup.viewmodel.SignUpViewModel
 import com.yourpackage.design_system.component.textField.GwangSanTextField
+
+
 
 @Composable
 internal fun PasswordSignupRoute(
     onBackClick: () -> Unit,
-    onCerTinSignUpClick: () -> Unit
+    onCerTinSignUpClick: () -> Unit,
+    viewModel: SignUpViewModel = hiltViewModel()
 ) {
+    val password by viewModel.password.collectAsStateWithLifecycle()
+    val rePassword by viewModel.checkPassword.collectAsStateWithLifecycle()
 
-    var password by remember { mutableStateOf("") }
-    var rePassword by remember { mutableStateOf("") }
+    val isPasswordValidError = password.isNotBlank() && !isValidPassword(password)
+    val isPasswordMismatchError = rePassword.isNotBlank() && password != rePassword
 
+    val isNextEnabled = !isPasswordValidError && !isPasswordMismatchError &&
+            password.isNotBlank() && rePassword.isNotBlank()
 
     PasswordSignupScreen(
         onBackClick = onBackClick,
         onCerTinSignUpClick = onCerTinSignUpClick,
-        isPasswordValidError = false,
-        isPasswordMismatchError = false,
+        isPasswordValidError = isPasswordValidError,
+        isPasswordMismatchError = isPasswordMismatchError,
+        isNextEnabled = isNextEnabled,
         password = password,
         rePassword = rePassword,
-        onPasswordChange = {password = it},
-        onRePasswordChange = {rePassword = it},
-        signInCallBack = {}
+        onPasswordChange = viewModel::onPasswordChange,
+        onRePasswordChange = viewModel::onCheckPasswordChange,
     )
 }
 
@@ -64,19 +78,17 @@ private fun PasswordSignupScreen(
     modifier: Modifier = Modifier,
     isPasswordValidError: Boolean,
     isPasswordMismatchError: Boolean,
+    isNextEnabled: Boolean,
     password: String,
     onBackClick: () -> Unit,
     onCerTinSignUpClick: () -> Unit,
     rePassword: String,
-    signInCallBack: () -> Unit,
     focusManager: FocusManager = LocalFocusManager.current,
     scrollState: ScrollState = rememberScrollState(),
     onPasswordChange: (String) -> Unit,
     onRePasswordChange: (String) -> Unit,
-
 ) {
     GwangSanTheme { colors, typography ->
-
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceBetween,
@@ -92,8 +104,7 @@ private fun PasswordSignupScreen(
                 .verticalScroll(scrollState)
                 .pointerInput(Unit) {
                     detectTapGestures {
-                        focusManager.clearFocus()
-                    }
+                        focusManager.clearFocus() }
                 }
         ) {
             Column(
@@ -104,17 +115,14 @@ private fun PasswordSignupScreen(
                     horizontalArrangement = Arrangement.Start,
                     modifier = modifier
                         .fillMaxWidth()
-                        .padding(
-                            top =18.dp,
-                            bottom = 32.dp
-                        )
-                    ,
-                ){
+                        .padding(top = 18.dp, bottom = 32.dp),
+                ) {
                     GwangSanTopBar(
                         startIcon = { DownArrowIcon(modifier = Modifier.GwangSanClickable { onBackClick() }) },
                         betweenText = "뒤로"
                     )
                 }
+
                 Text(
                     text = "회원가입",
                     style = typography.titleMedium2,
@@ -125,7 +133,7 @@ private fun PasswordSignupScreen(
                 Spacer(modifier = Modifier.height(6.dp))
 
                 Text(
-                    text = "비밀번호을 입력해주세요",
+                    text = "비밀번호를 입력해주세요",
                     style = typography.label,
                     color = colors.black.copy(alpha = 0.5f),
                     fontWeight = FontWeight.Normal
@@ -142,7 +150,7 @@ private fun PasswordSignupScreen(
                     placeHolder = "비밀번호",
                     isError = isPasswordValidError,
                     isDisabled = false,
-                    errorText = "유효하지 않은 빌밀번호입니다.",
+                    errorText = "영문과 숫자를 포함한 8자 이상 입력해주세요.",
                     onTextChange = onPasswordChange,
                 )
 
@@ -156,43 +164,26 @@ private fun PasswordSignupScreen(
                     placeHolder = "비밀번호 재입력",
                     isError = isPasswordMismatchError,
                     isDisabled = false,
-                    errorText = "비밀번호가 틀립니다.",
+                    errorText = "비밀번호가 일치하지 않습니다.",
                     onTextChange = onRePasswordChange,
                     label = "비밀번호를 다시 입력해주세요"
                 )
             }
+
             Column(
                 verticalArrangement = Arrangement.Bottom,
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(bottom = 64.dp), // 필요 시 여백 추가
-
+                    .padding(bottom = 64.dp),
             ) {
                 GwangSanStateButton(
                     text = "다음",
-                    state = if (password.isNotBlank() && rePassword.isNotBlank()) ButtonState.Enable else ButtonState.Disable,
+                    state = if (isNextEnabled) ButtonState.Enable else ButtonState.Disable,
                     modifier = Modifier.fillMaxWidth()
                 ) {
-                    signInCallBack()
                     onCerTinSignUpClick()
                 }
             }
         }
     }
-}
-
-@Preview
-@Composable
-fun PasswordSignupScreenPreview(){
-    PasswordSignupScreen(
-        isPasswordValidError = false,
-        isPasswordMismatchError = false,
-        password = "",
-        rePassword = "",
-        onPasswordChange = {},
-        onRePasswordChange = {},
-        signInCallBack = {},
-        onBackClick = {},
-        onCerTinSignUpClick = {}
-    )
 }
