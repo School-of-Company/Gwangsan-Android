286ca7a18136fd90e6d0a869db02b20d2cc082dc
jsw9436 <jswjsw101010@gmail.com>
2025-06-26 01:22:21 +0900

    :recycle: :: TextField 크기 수정

--- feature/signup/src/main/java/com/school_of_company/signup/view/CertinSignUpScreen.kt
+++ feature/signup/src/main/java/com/school_of_company/signup/view/CertinSignUpScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -169,7 +170,6 @@ private fun CertinSignUpScreen(
             modifier = modifier
                 .fillMaxSize()
                 .background(color = colors.white)
-                .imePadding()
                 .padding(24.dp)
                 .verticalScroll(scrollState)
                 .pointerInput(Unit) {
@@ -216,7 +216,9 @@ private fun CertinSignUpScreen(
                     errorText = "",
                     onTextChange = onPhoneNumberChange,
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                    modifier = Modifier.weight(1f)
+                    modifier = Modifier
+                        .height(70.dp)
+                        .width(250.dp)
                 )
 
                 GwangSanStateButton(
