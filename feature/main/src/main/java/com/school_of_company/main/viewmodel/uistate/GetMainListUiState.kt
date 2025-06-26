fc8563b830648b29f9073e0ebff3ca1fa0896ac2
kimjeakwan <s24042@gsm.hs.kr>
2025-06-26 23:47:52 +0900

    :sparkles: :: GetMainListUiState.kt

---
+++ feature/main/src/main/java/com/school_of_company/main/viewmodel/uistate/GetMainListUiState.kt
@@ -0,0 +1,11 @@
+package com.school_of_company.main.viewmodel.uistate
+
+import com.school_of_company.model.main.response.MainListResponseModel
+
+sealed interface GetMainListUiState {
+    data object Loading : GetMainListUiState
+    data object Empty : GetMainListUiState
+    data class Success(val getMainListResponse : List<MainListResponseModel>) :
+        GetMainListUiState
+    data class Error(val exception: Throwable) : GetMainListUiState
+}
