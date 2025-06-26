84623172f698a988ed230be6a60f84bd3ca2d08d
jsw9436 <jswjsw101010@gmail.com>
2025-06-27 05:55:42 +0900

    :recycle: :: enum 수정

--- core/model/src/main/java/com/school_of_company/model/enum/Dong.kt
+++ core/model/src/main/java/com/school_of_company/model/enum/Dong.kt
@@ -5,5 +5,10 @@ enum class Dong {
     ROLE_DONG_2,
     ROLE_DONG_3,
     ROLE_DONG_4,
-    ROLE_DONG_5,
+    ROLE_DONG_5;
+
+    companion object {
+        fun from(value: String): Dong = entries.firstOrNull { it.name == value }
+            ?: ROLE_DONG_1
+    }
 }
