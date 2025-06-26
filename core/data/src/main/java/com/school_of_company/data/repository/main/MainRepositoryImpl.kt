5697716e22eb7b135bd2c166c374a35ef35bd724
kimjeakwan <s24042@gsm.hs.kr>
2025-06-26 23:50:55 +0900

    :sparkles: :: MainRepositoryImpl.kt

---
+++ core/data/src/main/java/com/school_of_company/data/repository/main/MainRepositoryImpl.kt
@@ -0,0 +1,34 @@
+package com.school_of_company.data.repository.main
+
+import android.util.Log
+import com.school_of_company.data.repository.auth.AuthRepository
+import com.school_of_company.datastore.datasource.AuthTokenDataSource
+import com.school_of_company.model.enum.Mode
+import com.school_of_company.model.enum.Type
+import com.school_of_company.model.main.response.MainListResponseModel
+import com.school_of_company.network.datasource.auth.AuthDataSource
+import com.school_of_company.network.datasource.main.MainDataSource
+import com.school_of_company.network.mapper.main.reponse.toModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.transform
+import javax.inject.Inject
+
+
+class MainRepositoryImpl @Inject constructor(
+    private val mainDataSource: MainDataSource,
+) : MainRepository {
+    override fun allPostGeT(
+        type: Type,
+        mode: Mode
+    ): Flow<List<MainListResponseModel>> {
+        Log.d("MainRepositoryImpl", "📡 allPostGeT called with type=$type, mode=$mode")
+
+        return mainDataSource.allPostGet(
+            type = type,
+            mode = mode,
+        ).transform { response ->
+            Log.d("MainRepositoryImpl", "📦 Response size: ${response.size}")
+            emit(response.map { it.toModel() })
+        }
+    }
+}
