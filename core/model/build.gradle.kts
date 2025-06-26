951255bf95745d82585b0d0da74b41ae47fbcb1a
kimjeakwan <s24042@gsm.hs.kr>
2025-06-26 23:47:02 +0900

    :recycle: :: JSON 의존성 추가

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -5,4 +5,12 @@ plugins {
 dependencies {
     implementation(libs.kotlinx.datetime)
     implementation(libs.androidx.runtime.v161)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp.logging)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.retrofit.moshi.converter)
+    implementation(libs.moshi)
+    ksp(libs.retrofit.moshi.codegen)
 }
