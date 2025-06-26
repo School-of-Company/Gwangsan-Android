b59ec0de1e99f99d7b660b0325a4c3d625fc640b
jsw9436 <jswjsw101010@gmail.com>
2025-06-27 05:55:29 +0900

    :recycle: :: enum 수정

--- core/model/src/main/java/com/school_of_company/model/enum/Branch.kt
+++ core/model/src/main/java/com/school_of_company/model/enum/Branch.kt
@@ -2,5 +2,10 @@ package com.school_of_company.model.enum
 
 enum class Branch {
     ROLE_STUDENT,
-    ROLE_STUDENT_COUNCIL
+    ROLE_STUDENT_COUNCIL;
+
+    companion object {
+        fun from(value: String): Branch = entries.firstOrNull { it.name == value }
+            ?: ROLE_STUDENT
+    }
 }
