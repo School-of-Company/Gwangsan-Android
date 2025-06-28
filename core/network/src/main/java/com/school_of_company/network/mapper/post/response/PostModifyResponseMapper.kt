fa75de0b4aaa327629a053ac0c60d3eb4a304082
jsw9436 <jswjsw101010@gmail.com>
2025-06-29 02:02:14 +0900

    :sparkles: :: add PostModifyResponseMapper

---
+++ core/network/src/main/java/com/school_of_company/network/mapper/post/response/PostModifyResponseMapper.kt
@@ -0,0 +1,9 @@
+package com.school_of_company.network.mapper.post.response
+
+import com.school_of_company.model.post.response.PostModifyResponseModel
+import com.school_of_company.network.dto.post.response.PostModifyResponse
+
+fun PostModifyResponse.toModel(): PostModifyResponseModel
+ = PostModifyResponseModel(
+    message = message
+)
