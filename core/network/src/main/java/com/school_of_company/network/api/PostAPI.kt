0ffc073af2fe93dce30cbf8341a0d7bda8530917
jsw9436 <jswjsw101010@gmail.com>
2025-06-29 18:41:09 +0900

    :recycle: :: remove header

--- core/network/src/main/java/com/school_of_company/network/api/PostAPI.kt
+++ core/network/src/main/java/com/school_of_company/network/api/PostAPI.kt
@@ -3,7 +3,6 @@ package com.school_of_company.network.api
 import com.school_of_company.model.enum.Mode
 import com.school_of_company.model.enum.Type
 import com.school_of_company.network.dto.post.request.PostWriteRequest
-import com.school_of_company.network.dto.post.response.PostModifyResponse
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
@@ -13,7 +12,6 @@ interface PostAPI {
 
     @POST("/api/post")
     suspend fun writePostInformation(
-        @Header("Authorization") token: String,
         @Query("type") type: Type,
         @Query("mode") mode: Mode,
         @Body request: PostWriteRequest
@@ -21,9 +19,8 @@ interface PostAPI {
 
     @POST("/api/post/modify")
     suspend fun modifyPostInformation(
-        @Header("Authorization") token: String,
         @Query("type") type: Type,
         @Query("mode") mode: Mode,
         @Body request: PostWriteRequest
-    ): PostModifyResponse
+    )
 }
