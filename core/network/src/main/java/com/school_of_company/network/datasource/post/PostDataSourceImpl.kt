9751125829a0a67523aa28d89a2ad87242c97c68
jsw9436 <jswjsw101010@gmail.com>
2025-06-29 18:41:51 +0900

    :recycle: :: remove token

--- core/network/src/main/java/com/school_of_company/network/datasource/post/PostDataSourceImpl.kt
+++ core/network/src/main/java/com/school_of_company/network/datasource/post/PostDataSourceImpl.kt
@@ -4,7 +4,6 @@ import com.school_of_company.model.enum.Mode
 import com.school_of_company.model.enum.Type
 import com.school_of_company.network.api.PostAPI
 import com.school_of_company.network.dto.post.request.PostWriteRequest
-import com.school_of_company.network.dto.post.response.PostModifyResponse
 import com.school_of_company.network.util.performApiRequest
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -14,22 +13,20 @@ class PostDataSourceImpl @Inject constructor(
 ) : PostDataSource {
 
     override fun writePostInformation(
-        token: String,
         type: Type,
         mode: Mode,
         request: PostWriteRequest
     ): Flow<Unit> =
         performApiRequest {
-            postAPI.writePostInformation(token, type, mode, request)
+            postAPI.writePostInformation(type, mode, request)
         }
 
     override fun modifyPostInformation(
-        token: String,
         type: Type,
         mode: Mode,
         request: PostWriteRequest
-    ): Flow<PostModifyResponse> =
+    ): Flow<Unit> =
         performApiRequest {
-            postAPI.modifyPostInformation(token, type, mode, request)
+            postAPI.modifyPostInformation(type, mode, request)
         }
 }
