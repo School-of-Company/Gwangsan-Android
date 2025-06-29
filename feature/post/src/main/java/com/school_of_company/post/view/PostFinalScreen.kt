d177436f94ba1f3e429f7651b780a130c89f89ee
jsw9436 <jswjsw101010@gmail.com>
2025-06-30 00:11:27 +0900

    :fire: :: remove unused import

--- feature/post/src/main/java/com/school_of_company/post/view/PostFinalScreen.kt
+++ feature/post/src/main/java/com/school_of_company/post/view/PostFinalScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.school_of_company.design_system.component.progress.GwangSanTopBarProgress
