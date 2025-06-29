b22808ee8858268bd93b0b655ead46ddf80a8740
jsw9436 <jswjsw101010@gmail.com>
2025-06-29 18:45:46 +0900

    :fire: :: remove unused import

--- core/network/src/main/java/com/school_of_company/network/api/PostAPI.kt
+++ core/network/src/main/java/com/school_of_company/network/api/PostAPI.kt
@@ -4,7 +4,6 @@ import com.school_of_company.model.enum.Mode
 import com.school_of_company.model.enum.Type
 import com.school_of_company.network.dto.post.request.PostWriteRequest
 import retrofit2.http.Body
-import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Query
 
