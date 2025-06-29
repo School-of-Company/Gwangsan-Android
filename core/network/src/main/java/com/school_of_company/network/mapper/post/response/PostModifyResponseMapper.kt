c2962c581c7658628a8638e6f7460938564f625d
jsw9436 <jswjsw101010@gmail.com>
2025-06-29 18:42:36 +0900

    :fire: :: PostModifyResponseMapper.kt

--- core/network/src/main/java/com/school_of_company/network/mapper/post/response/PostModifyResponseMapper.kt
+++
@@ -1,9 +0,0 @@
-package com.school_of_company.network.mapper.post.response
-
-import com.school_of_company.model.post.response.PostModifyResponseModel
-import com.school_of_company.network.dto.post.response.PostModifyResponse
-
-fun PostModifyResponse.toModel(): PostModifyResponseModel
- = PostModifyResponseModel(
-    message = message
-)
