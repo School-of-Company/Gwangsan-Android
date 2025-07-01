6cc67d417b60adcbdec9cf7350301f6651a9f5c6
kimjeakwan <s24042@gsm.hs.kr>
2025-07-01 17:00:23 +0900

    :recycle: :: 로그인 요청용 LoginRequestModel 변경

--- core/model/src/main/java/com/school_of_company/model/auth/request/LoginRequestModel.kt
+++ core/model/src/main/java/com/school_of_company/model/auth/request/LoginRequestModel.kt
@@ -1,6 +1,12 @@
 package com.school_of_company.model.auth.request
 
+import com.school_of_company.model.enum.OsType
+import java.util.UUID
+
 data class LoginRequestModel(
     val nickname: String,
     val password: String,
+    val deviceToken: String,
+    val deviceId: String,
+    val osType: OsType = OsType.ANDROID
 )
