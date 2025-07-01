478e82213f6288a81e91fe6eee297bf0915a367b
kimjeakwan <s24042@gsm.hs.kr>
2025-07-01 16:58:01 +0900

    :heavy_minus_sign: :: firebase 의존성 삭제

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,15 +40,12 @@ dependencies {
     implementation(project(":core:data"))
     implementation(project(":core:network"))
     implementation(project(":core:common"))
-
     implementation(project(":feature:post"))
     implementation(project(":feature:main"))
     implementation(libs.androidx.window.size)
     implementation(libs.androidx.navigation.runtime)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.material3)
-    implementation(libs.firebase.messaging.ktx)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext)
 
