acef5b32bb42f14a74705d65b07ee7b9f94fc616
kimjeakwan <s24042@gsm.hs.kr>
2025-07-01 17:48:33 +0900

    :recycle: :: enum 대신 문자열로 변경

--- core/network/src/main/java/com/school_of_company/network/dto/auth/requset/LoginRequest.kt
+++ core/network/src/main/java/com/school_of_company/network/dto/auth/requset/LoginRequest.kt
@@ -1,10 +1,7 @@
 package com.school_of_company.network.dto.auth.requset
 
-import android.content.Context
-import com.school_of_company.model.enum.OsType
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import java.util.UUID
 
 @JsonClass(generateAdapter = true)
 data class LoginRequest(
@@ -12,5 +9,5 @@ data class LoginRequest(
     @Json(name = "password") val password: String,
     @Json(name = "deviceToken") val deviceToken: String,
     @Json(name = "deviceId") val deviceId: String,
-    @Json(name = "osType") val osType: OsType = OsType.ANDROID // 고정
+    @Json(name = "osType") val osType: String = "ANDROID"
 )
