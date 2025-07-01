a21ef3c38ab59aa59ea080803697df03c652b5ca
kimjeakwan <s24042@gsm.hs.kr>
2025-07-01 16:59:59 +0900

    :recycle: :: 로그인 요청용 LoginRequest DTO 변경

--- core/network/src/main/java/com/school_of_company/network/dto/auth/requset/LoginRequest.kt
+++ core/network/src/main/java/com/school_of_company/network/dto/auth/requset/LoginRequest.kt
@@ -1,10 +1,16 @@
 package com.school_of_company.network.dto.auth.requset
 
+import android.content.Context
+import com.school_of_company.model.enum.OsType
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import java.util.UUID
 
 @JsonClass(generateAdapter = true)
 data class LoginRequest(
     @Json(name = "nickname") val nickname: String,
-    @Json(name = "password") val password: String
+    @Json(name = "password") val password: String,
+    @Json(name = "deviceToken") val deviceToken: String,
+    @Json(name = "deviceId") val deviceId: String,
+    @Json(name = "osType") val osType: OsType = OsType.ANDROID // 고정
 )
