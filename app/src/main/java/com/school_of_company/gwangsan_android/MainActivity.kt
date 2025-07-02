c2eef9f1258085ac960f081902750e97445d80c1
명훈 <s23065@gsm.hs.kr>
2025-07-02 23:39:09 +0900

    :sparkles: :: Add Fetch DeviceToken Logic - MainActivity

--- app/src/main/java/com/school_of_company/gwangsan_android/MainActivity.kt
+++ app/src/main/java/com/school_of_company/gwangsan_android/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.school_of_company.gwangsan_android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.lifecycle.lifecycleScope
 import com.school_of_company.gwangsan_android.ui.GwangSanApp
 import com.school_of_company.design_system.theme.GwangSanTheme
+import com.school_of_company.device.manager.DeviceTokenManager
 import com.school_of_company.signin.navigation.StartRoute
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var deviceTokenManager: DeviceTokenManager
 
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +38,10 @@ class MainActivity : ComponentActivity() {
                         windowSizeClass = calculateWindowSizeClass(activity = this))
                 }
             }
+        }
+
+        lifecycleScope.launch {
+            deviceTokenManager.fetchDeviceToken()
         }
     }
 }
