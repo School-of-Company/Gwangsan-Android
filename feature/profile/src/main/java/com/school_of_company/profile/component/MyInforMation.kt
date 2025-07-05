7c200e25ac7d711cb104e7c7669855b0632436df
명훈 <s23065@gsm.hs.kr>
2025-07-06 04:01:13 +0900

    :recycle: :: Add Logout Param - MyInforMation

--- feature/profile/src/main/java/com/school_of_company/profile/component/MyInforMation.kt
+++ feature/profile/src/main/java/com/school_of_company/profile/component/MyInforMation.kt
@@ -1,7 +1,6 @@
 package com.school_of_company.profile.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,25 +10,22 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.school_of_company.design_system.componet.button.GwangSanEnableButton
 import com.school_of_company.design_system.componet.clickable.GwangSanClickable
+import com.school_of_company.design_system.componet.icons.ArrowDown
 import com.school_of_company.design_system.componet.icons.EllipseIcon
-import com.school_of_company.design_system.componet.icons.LeftArrowIcon
 import com.school_of_company.design_system.theme.GwangSanTheme
-import com.school_of_company.profile.view.MyProfileRoute
-
 
 @Composable
 internal fun MyInformation(
     modifier: Modifier = Modifier,
-    onModifyClick: () -> Unit
+    onModifyClick: () -> Unit,
+    onLogoutClick: () -> Unit
 ) {
     GwangSanTheme { colors, typography ->
 
@@ -62,9 +58,9 @@ internal fun MyInformation(
                             color = colors.gray500
                         )
 
-                        Spacer(modifier = Modifier.width(4.dp))
+                        Spacer(modifier = Modifier.width(12.dp))
 
-                        LeftArrowIcon(modifier = Modifier.GwangSanClickable {  })
+                        ArrowDown(modifier = Modifier.GwangSanClickable { onLogoutClick() })
                     }
                 }
             }
@@ -82,6 +78,7 @@ private fun PreviewMyInformation(
 
 ){
     MyInformation(
-        onModifyClick = {}
+        onModifyClick = {},
+        onLogoutClick = {}
     )
 }
