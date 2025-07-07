78192d8121709c08dd49bcd751617ebaa42a3492
명훈 <s23065@gsm.hs.kr>
2025-07-07 23:26:55 +0900

    :recycle: :: Apply API Changed - PostDataSource(Impl)

--- core/network/src/main/java/com/school_of_company/network/datasource/post/PostDataSourceImpl.kt
+++ core/network/src/main/java/com/school_of_company/network/datasource/post/PostDataSourceImpl.kt
@@ -3,7 +3,9 @@ package com.school_of_company.network.datasource.post
 import com.school_of_company.model.enum.Mode
 import com.school_of_company.model.enum.Type
 import com.school_of_company.network.api.PostAPI
-import com.school_of_company.network.dto.post.request.PostWriteRequest
+import com.school_of_company.network.dto.post.request.PostAllRequest
+import com.school_of_company.network.dto.post.response.PostListResponse
+import com.school_of_company.network.dto.post.response.PostModifyResponse
 import com.school_of_company.network.util.performApiRequest
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -11,22 +13,34 @@ import javax.inject.Inject
 class PostDataSourceImpl @Inject constructor(
     private val postAPI: PostAPI
 ) : PostDataSource {
+    override fun writePostInformation(body: PostAllRequest): Flow<Unit> =
+        performApiRequest { postAPI.writePostInformation(body = body) }
 
-    override fun writePostInformation(
-        type: Type,
-        mode: Mode,
-        request: PostWriteRequest
-    ): Flow<Unit> =
-        performApiRequest {
-            postAPI.writePostInformation(type, mode, request)
-        }
 
     override fun modifyPostInformation(
-        type: Type,
-        mode: Mode,
-        request: PostWriteRequest
-    ): Flow<Unit> =
-        performApiRequest {
-            postAPI.modifyPostInformation(type, mode, request)
-        }
+        postId: Long,
+        body: PostAllRequest
+    ): Flow<PostModifyResponse> =
+        performApiRequest { postAPI.modifyPostInformation(
+            postId = postId,
+            body = body
+        ) }
+
+    override fun getSpecificInformation(): Flow<PostListResponse> =
+        performApiRequest { postAPI.getSpecificInformation() }
+
+    override fun getAllPostInformation(type: String, mode: String): Flow<PostListResponse> =
+        performApiRequest { postAPI.getAllPostInformation(
+            type = type,
+            mode = mode
+        ) }
+
+    override fun getMyPostInformation(type: String, mode: String): Flow<PostListResponse> =
+        performApiRequest { postAPI.getMyPostInformation(
+            type = type,
+            mode = mode
+        ) }
+
+    override fun deletePostInformation(postId: Long): Flow<Unit> =
+        performApiRequest { postAPI.deletePostInformation(postId = postId) }
 }
