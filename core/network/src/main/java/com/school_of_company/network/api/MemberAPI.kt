511f06cebdd305e2ab4a03618adf4726b28ae7b9
명훈 <s23065@gsm.hs.kr>
2025-07-09 08:04:32 +0900

    :sparkles: :: Add MemberAPI(Retrofit)

---
+++ core/network/src/main/java/com/school_of_company/network/api/MemberAPI.kt
@@ -0,0 +1,28 @@
+package com.school_of_company.network.api
+
+import com.school_of_company.network.dto.member.request.ModifyMemberInformationRequest
+import com.school_of_company.network.dto.member.response.GetAllMemberResponse
+import com.school_of_company.network.dto.member.response.GetMemberResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.Path
+
+interface MemberAPI {
+
+    @GET("/api/member")
+    suspend fun getMyProfileInformation() : GetMemberResponse
+
+    @PATCH("/api/member")
+    suspend fun editMyProfileInformation(
+        @Body body: ModifyMemberInformationRequest
+    )
+
+    @GET("/api/member/{memberId}")
+    suspend fun getSpecificMemberProfileInformation(
+        @Path("memberId") memberId: Long
+    ) : GetAllMemberResponse
+
+    @GET("/api/member/all")
+    suspend fun getAllMemberProfileInformation() : List<GetAllMemberResponse>
+}
