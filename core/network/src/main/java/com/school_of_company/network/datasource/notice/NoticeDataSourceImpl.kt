8519913e6fb260c3254215603ed5b68399f2e6f7
명훈 <s23065@gsm.hs.kr>
2025-07-10 14:02:01 +0900

    :sparkles: :: Add NoticeDataSource(Impl)

---
+++ core/network/src/main/java/com/school_of_company/network/datasource/notice/NoticeDataSourceImpl.kt
@@ -0,0 +1,18 @@
+package com.school_of_company.network.datasource.notice
+
+import com.school_of_company.network.api.NoticeAPI
+import com.school_of_company.network.dto.notice.response.GetAllNoticeResponse
+import com.school_of_company.network.dto.notice.response.GetSpecificNoticeResponse
+import com.school_of_company.network.util.performApiRequest
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class NoticeDataSourceImpl @Inject constructor(
+    private val noticeApi: NoticeAPI
+) : NoticeDataSource {
+    override fun getAllNotice(): Flow<List<GetAllNoticeResponse>> =
+        performApiRequest { noticeApi.getAllNotice() }
+
+    override fun getSpecificNotice(noticeId: Long): Flow<List<GetSpecificNoticeResponse>> =
+        performApiRequest { noticeApi.getSpecificNotice(noticeId = noticeId) }
+}
