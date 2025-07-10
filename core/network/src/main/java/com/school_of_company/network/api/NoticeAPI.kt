cf0d35c753404dd06e2fe4e831a4ac10dbc9bf60
명훈 <s23065@gsm.hs.kr>
2025-07-10 14:07:26 +0900

    :sparkles: :: Add NoticeAPI

---
+++ core/network/src/main/java/com/school_of_company/network/api/NoticeAPI.kt
@@ -0,0 +1,17 @@
+package com.school_of_company.network.api
+
+import com.school_of_company.network.dto.notice.response.GetAllNoticeResponse
+import com.school_of_company.network.dto.notice.response.GetSpecificNoticeResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface NoticeAPI {
+
+    @GET("/api/notice")
+    suspend fun getAllNotice() : List<GetAllNoticeResponse>
+
+    @GET("/api/notice/{noticeId}")
+    suspend fun getSpecificNotice(
+        @Path("noticeId") noticeId : Long
+    ) : List<GetSpecificNoticeResponse>
+}
