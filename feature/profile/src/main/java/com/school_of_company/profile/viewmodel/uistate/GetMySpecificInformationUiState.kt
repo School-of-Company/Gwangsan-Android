73fc4cf3442ca751711c64b7a093ad0604c2b575
kimjeakwan <s24042@gsm.hs.kr>
2025-07-11 03:22:33 +0900

    :sparkles: :: GetMySpecificInformationUiState.kt

---
+++ feature/profile/src/main/java/com/school_of_company/profile/viewmodel/uistate/GetMySpecificInformationUiState.kt
@@ -0,0 +1,10 @@
+package com.school_of_company.profile.viewmodel.uistate
+
+import com.school_of_company.model.post.response.Post
+
+sealed interface GetMySpecificInformationUiState {
+    data object Loading : GetMySpecificInformationUiState
+    data object Empty : GetMySpecificInformationUiState
+    data class Success(val data: List<Post>) : GetMySpecificInformationUiState
+    data class Error(val exception: Throwable) : GetMySpecificInformationUiState
+}
