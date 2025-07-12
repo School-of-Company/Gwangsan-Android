514b45ab30bea800c6519e6033519404c51e0350
명훈 <s23065@gsm.hs.kr>
2025-07-13 05:56:59 +0900

    :recycle: :: Refactor ReportType Member Value

--- core/model/src/main/java/com/school_of_company/model/enum/ReportType.kt
+++ core/model/src/main/java/com/school_of_company/model/enum/ReportType.kt
@@ -3,6 +3,6 @@ package com.school_of_company.model.enum
 enum class ReportType(val value: String) {
     BAD_LANGUAGE("욕설/비방"),
     FRAUD("사기"),
-    MEMBER("허위 매물 등록"),
+    MEMBER("사용자 신고"),
     ETC("기타")
 }
