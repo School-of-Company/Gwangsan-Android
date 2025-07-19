410848ebbe8b7c365f73cd1c8bf5dad8f76a4674
명훈 <s23065@gsm.hs.kr>
2025-07-19 23:37:41 +0900

    :sparkles: :: Add FirebaseModule

---
+++ core/firebase/src/main/java/com/school_of_company/firebase/di/FirebaseModule.kt
@@ -0,0 +1,21 @@
+package com.school_of_company.firebase.di
+
+import com.school_of_company.firebase.reporter.CrashReporter
+import com.school_of_company.network.api.WebhookAPI
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Provides
+    @Singleton
+    fun provideCrashReporter(webhookAPI: WebhookAPI): CrashReporter {
+        return CrashReporter(webhookAPI)
+    }
+}
