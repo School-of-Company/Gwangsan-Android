496564ea2fe6914b8daeed72f63c53626b63c979
명훈 <s23065@gsm.hs.kr>
2025-07-22 06:36:46 +0900

    :sparkles: :: Add ChatAPI

---
+++ core/network/src/main/java/com/school_of_company/network/api/ChatAPI.kt
@@ -0,0 +1,41 @@
+package com.school_of_company.network.api
+
+import com.school_of_company.network.dto.chat.request.ReadMessageRequest
+import com.school_of_company.network.dto.chat.response.GetChatMessageResponse
+import com.school_of_company.network.dto.chat.response.GetChatRoomResponse
+import com.school_of_company.network.dto.chat.response.JoinChatResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ChatAPI {
+
+    @POST("/api/chat/room/{product_id}")
+    suspend fun createChatRoom(
+        @Path("product_id") productId: Long
+    ) : JoinChatResponse
+
+    @GET("/api/chat/room/{product_id}")
+    suspend fun joinChatRoom(
+        @Path("product_id") productId: Long
+    ) : JoinChatResponse
+
+    @GET("/api/chat/rooms")
+    suspend fun getChatRoomList() : List<GetChatRoomResponse>
+
+    @GET("/api/chat/{room_id}")
+    suspend fun getChatMessageList(
+        @Path("roomId") roomId: Long,
+        @Query("lastCreatedAt") lastCreatedAt: String? = null,
+        @Query("lastMessageId") lastMessageId: String? = null,
+        @Query("limit") limit: Int = 20
+    ) : List<GetChatMessageResponse>
+
+    @PATCH("/api/chat/read")
+    suspend fun readChatMessage(
+        @Body body: ReadMessageRequest
+    )
+}
