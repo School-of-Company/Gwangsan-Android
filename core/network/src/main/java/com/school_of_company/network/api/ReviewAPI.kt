e8eadff25b518993ffc438bb772b992a72480110
kimjeakwan <s24042@gsm.hs.kr>
2025-07-21 17:17:41 +0900

    :recycle: :: list로 변경

--- core/network/src/main/java/com/school_of_company/network/api/ReviewAPI.kt
+++ core/network/src/main/java/com/school_of_company/network/api/ReviewAPI.kt
@@ -23,5 +23,5 @@ interface ReviewAPI {
     @GET("/api/review/{memberId}")
     suspend fun getOtherReview(
         @Path("memberId") memberId: Long
-    ) : ReviewResponse
+    ) : List<ReviewResponse>
 }
