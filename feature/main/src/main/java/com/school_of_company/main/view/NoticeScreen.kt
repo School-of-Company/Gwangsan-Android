629ef513724f9d312d17ac011ad1b0357d03b2cc
kimjeakwan <s24042@gsm.hs.kr>
2025-07-22 17:43:12 +0900

    :recycle: :: 알람 조회 기능

--- feature/main/src/main/java/com/school_of_company/main/view/NoticeScreen.kt
+++ feature/main/src/main/java/com/school_of_company/main/view/NoticeScreen.kt
@@ -1,6 +1,7 @@
 package com.school_of_company.main.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,60 +9,60 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.SwipeRefreshIndicator
+import com.google.accompanist.swiperefresh.SwipeRefreshState
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.school_of_company.design_system.component.clickable.GwangSanClickable
 import com.school_of_company.design_system.component.icons.CloseIcon
 import com.school_of_company.design_system.component.topbar.GwangSanSubTopBar
 import com.school_of_company.design_system.theme.GwangSanTheme
-import com.school_of_company.main.component.Notice
 import com.school_of_company.main.component.NoticeList
+import com.school_of_company.main.viewmodel.MainViewModel
+import com.school_of_company.main.viewmodel.uistate.GetAlertUiState
+
 @Composable
 internal  fun NoticeRoute(
-    onBackClick: () -> Unit
+    onBackClick: () -> Unit,
+    viewModel: MainViewModel = hiltViewModel()
 ){
+    val swipeRefreshLoading by viewModel.swipeRefreshLoading.collectAsStateWithLifecycle(
+        initialValue = false
+    )
+
+    val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = swipeRefreshLoading)
+
+    val getAlertUiState by viewModel.getAlertUiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) {
+        viewModel.getAlert()
+    }
+
     NoticeScreen(
         onBackClick = onBackClick,
-        items = listOf(
-            Notice(
-                id = 1,
-                title = "📢 서비스 점검 안내",
-                content = "7월 20일 오전 2시부터 4시까지 서버 점검이 진행됩니다.",
-                image = null
-            ),
-            Notice(
-                id = 2,
-                title = "🎉 신규 기능 출시",
-                content = "채팅 기능과 알림 설정 기능이 새롭게 추가되었습니다!",
-                image = null
-            ),
-            Notice(
-                id = 3,
-                title = "🚨 개인정보 처리방침 변경 안내",
-                content = "개인정보 처리방침이 8월 1일부터 변경될 예정입니다. 꼭 확인해주세요.",
-                image = null
-            ),
-            Notice(
-                id = 4,
-                title = "📌 자주 묻는 질문 정리",
-                content = "많은 분들이 궁금해하시는 내용을 정리해두었습니다.",
-                image = null
-            ),
-            Notice(
-                id = 5,
-                title = "📦 배송 지연 안내",
-                content = "기상 악화로 인해 일부 지역의 배송이 지연되고 있습니다.",
-                image = null
-            )
-        )
+        swipeRefreshState = swipeRefreshState,
+        getAlertCallBack = {viewModel.getAlert()},
+        getAlertUiState = getAlertUiState
     )
 }
+
 @Composable
-fun NoticeScreen(
+private fun NoticeScreen(
     modifier: Modifier = Modifier,
-    items: List<Notice>,
+    getAlertUiState: GetAlertUiState,
+    swipeRefreshState: SwipeRefreshState,
+    getAlertCallBack: () -> Unit,
     onBackClick: () -> Unit,
 ) {
     GwangSanTheme { colors, typography ->
@@ -91,28 +92,81 @@ fun NoticeScreen(
 
                 Spacer(modifier = Modifier.height(24.dp))
 
-                NoticeList(
-                    modifier = Modifier.fillMaxSize(),
-                    items = items
-                )
+                SwipeRefresh(
+                    state = swipeRefreshState,
+                    onRefresh = { getAlertCallBack() },
+                    indicator = { state, refreshTrigger ->
+                        SwipeRefreshIndicator(
+                            state = state,
+                            refreshTriggerDistance = refreshTrigger,
+                            contentColor = colors.main500
+                        )
+                    }
+                ) {
+                    when (getAlertUiState) {
+                        is GetAlertUiState.Success -> {
+                            NoticeList(
+                                items = getAlertUiState.data,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(horizontal = 16.dp)
+                            )
+                        }
+
+                        is GetAlertUiState.Error -> {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .verticalScroll(rememberScrollState())
+                                    .background(color = colors.white)
+                            ) {
+                                Text(
+                                    text = "알림을 조회하는데 실패했습니다..",
+                                    style = typography.titleMedium2,
+                                    color = colors.gray500
+                                )
+                            }
+                        }
+
+                        is GetAlertUiState.Empty -> {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .verticalScroll(rememberScrollState())
+                                    .background(color = colors.white)
+                            ) {
+                                Text(
+                                    text = "알림 없습니다..",
+                                    style = typography.titleMedium2,
+                                    color = colors.gray500
+                                )
+                            }
+                        }
+
+                        is GetAlertUiState.Loading -> {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .verticalScroll(rememberScrollState())
+                                    .background(color = colors.white)
+                            ) {
+                                Text(
+                                    text = "알림 로딩중..",
+                                    style = typography.titleMedium2,
+                                    color = colors.gray500
+                                )
+                            }
+                        }
+                    }
+                }
 
             }
         }
     }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun NoticeScreenPreview() {
-    val dummyNotices = listOf(
-        Notice(id = 1, title = "📢 첫 번째 공지", content = "이것은 첫 번째 공지입니다.", image = null),
-        Notice(id = 2, title = "📌 두 번째 공지", content = "다음은 중요한 사항입니다.", image = null),
-        Notice(id = 3, title = "📎 세 번째 공지", content = "세 번째 공지의 내용입니다.", image = null)
-    )
-
-    NoticeScreen(
-        items = dummyNotices,
-        onBackClick = {},
-    )
 }
