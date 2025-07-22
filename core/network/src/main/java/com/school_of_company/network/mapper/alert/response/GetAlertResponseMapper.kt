2e121734af1b871bb087209a14e1b2fb1e0a1ac8
kimjeakwan <s24042@gsm.hs.kr>
2025-07-22 17:40:24 +0900

    :sparkles: :: GetAlertResponseMapper.kt

---
+++ core/network/src/main/java/com/school_of_company/network/mapper/alert/response/GetAlertResponseMapper.kt
@@ -0,0 +1,22 @@
+package com.school_of_company.network.mapper.alert.response
+
+import com.school_of_company.model.alert.response.GetAlertImagesModel
+import com.school_of_company.model.alert.response.GetAlertResponseModel
+import com.school_of_company.network.dto.alert.GetAlertImages
+import com.school_of_company.network.dto.alert.GetAlertResponse
+
+fun GetAlertResponse.toModel() = GetAlertResponseModel(
+    id = this.id,
+    title = this.title,
+    createdAt = this.createdAt,
+    content = this.content,
+    images = this.images.map { it.toModel() },
+    alertType = this.alertType,
+    place = this.place
+)
+
+fun GetAlertImages.toModel(): GetAlertImagesModel =
+    GetAlertImagesModel(
+        imageId = this.imageId,
+        imageUrl = this.imageUrl
+    )
