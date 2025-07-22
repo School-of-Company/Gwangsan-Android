b7b478dad116b24260fc2f6c57406db0313af2e6
kimjeakwan <s24042@gsm.hs.kr>
2025-07-22 22:39:02 +0900

    :sparkles: :: ImageUpLoadUiState.kt

---
+++ feature/content/src/main/java/com/school_of_company/content/viewmodel/uistate/ImageUpLoadUiState.kt
@@ -0,0 +1,9 @@
+package com.school_of_company.content.viewmodel.uistate
+
+import com.school_of_company.model.image.response.ImageUpLoadResponseModel
+
+interface ImageUpLoadUiState {
+    data object Loading : ImageUpLoadUiState
+    data class Success(val data: ImageUpLoadResponseModel) : ImageUpLoadUiState
+    data class Error(val exception: Throwable) : ImageUpLoadUiState
+}
