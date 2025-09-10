e1a5a0528e0b37843b1797f6d8e5f7bc152894a1
kimjeakwan <s24042@gsm.hs.kr>
2025-09-10 23:32:31 +0900

    :recycle: :: imePadding 추가

--- feature/signup/src/main/java/com/school_of_company/signup/view/NeighborhoodSignupScreen.kt
+++ feature/signup/src/main/java/com/school_of_company/signup/view/NeighborhoodSignupScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -92,6 +93,7 @@ private fun NeighborhoodSignupScreen(
                 .fillMaxSize()
                 .background(color = colors.white)
                 .verticalScroll(scrollState)
+                .imePadding()
                 .padding(top = 24.dp, start = 24.dp, end = 24.dp)
                 .pointerInput(Unit) {
                     detectTapGestures { focusManager.clearFocus() }
