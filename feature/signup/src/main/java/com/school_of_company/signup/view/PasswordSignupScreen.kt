31112297e5da6e2de2be2a3a2b645fc1b57a78a1
kimjeakwan <s24042@gsm.hs.kr>
2025-09-10 23:32:50 +0900

    :recycle: :: imePadding 추가

--- feature/signup/src/main/java/com/school_of_company/signup/view/PasswordSignupScreen.kt
+++ feature/signup/src/main/java/com/school_of_company/signup/view/PasswordSignupScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -103,6 +104,7 @@ private fun PasswordSignupScreen(
                     end = 24.dp
                 )
                 .verticalScroll(scrollState)
+                .imePadding()
                 .pointerInput(Unit) {
                     detectTapGestures {
                         focusManager.clearFocus() }
