0dad8db61a8e4f5651d22081010cbd60e2311fec
명훈 <s23065@gsm.hs.kr>
2025-11-10 10:51:22 +0900

    :recycle: :: Add NotFoundImage UiState((Modify)PostUiState)

--- feature/post/src/main/java/com/school_of_company/post/viewmodel/uiState/ModifyPostUiState.kt
+++ feature/post/src/main/java/com/school_of_company/post/viewmodel/uiState/ModifyPostUiState.kt
@@ -3,5 +3,7 @@ package com.school_of_company.post.viewmodel.uiState
 interface ModifyPostUiState {
     data object Loading : ModifyPostUiState
     data object Success : ModifyPostUiState
+    data object NotFoundImage : ModifyPostUiState
+
     data class Error(val exception: Throwable) : ModifyPostUiState
 }
